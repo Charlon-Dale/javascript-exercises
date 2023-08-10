0208ca3f9826452d7fade07088645d7df26c3100
Charlon-Dale <charlondalesanagustin@gmail.com>
2023-08-10 09:04:42 +0800

    still figuring out removeFromArray file

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr , value) {
+    arr = [];
 
+    for (let indexNum = 0; indexNum < arr.length; indexNum++) {
+        if (arr[indexNum] === value) {
+            arr.splice(arr[indexNum],1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
